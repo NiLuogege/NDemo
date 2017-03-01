9081f992efe126dc5a320e31b5b1c0dab22e8f4c
niluogege <835350313@qq.com>
2017-03-01 18:00:29 +0800

    添加了left content两个fragment

---
+++ NDemo/app/src/main/java/com/example/well/ndemo/ui/activity/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.well.ndemo.ui.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.FrameLayout;
+
+import com.example.well.ndemo.R;
+import com.example.well.ndemo.ui.fragment.ContentFragment;
+import com.example.well.ndemo.ui.fragment.LeftFragment;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity {
+    @Bind(R.id.fl_content)
+    FrameLayout mFlContent;
+    @Bind(R.id.fl_left)
+    FrameLayout mFlLeft;
+
+    private static final String TAG_CONTENTFRAGMENT="TagContentFragment";
+    private static final String TAG_LEFTFRAGMENT="TagLeftFragment";
+    private int contentId=R.id.fl_content;
+    private int leftId=R.id.fl_left;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        ContentFragment contentFragment = new ContentFragment();
+        getSupportFragmentManager().beginTransaction().add(contentId,contentFragment,TAG_CONTENTFRAGMENT).commitAllowingStateLoss();
+        LeftFragment leftFragment = new LeftFragment();
+        getSupportFragmentManager().beginTransaction().add(leftId,leftFragment,TAG_LEFTFRAGMENT).commitAllowingStateLoss();
+    }
+}
