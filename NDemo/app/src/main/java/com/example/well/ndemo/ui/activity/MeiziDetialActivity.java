d26cece748c2f78ec9321963a1d4df65eee134d4
niluogege <835350313@qq.com>
2017-03-08 16:44:57 +0800

    弄懂了style中的配置

--- NDemo/app/src/main/java/com/example/well/ndemo/ui/activity/MeiziDetialActivity.java
+++ NDemo/app/src/main/java/com/example/well/ndemo/ui/activity/MeiziDetialActivity.java
@@ -1,8 +1,14 @@
 package com.example.well.ndemo.ui.activity;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.view.animation.LinearOutSlowInInterpolator;
+import android.support.v7.widget.Toolbar;
+import android.transition.Transition;
+import android.util.Log;
 
+import com.example.well.ndemo.BuildConfig;
 import com.example.well.ndemo.R;
 import com.example.well.ndemo.view.ElasticDragDismissFrameLayout;
 
@@ -17,6 +23,8 @@ import butterknife.ButterKnife;
 public class MeiziDetialActivity extends BaseActivity {
     @Bind(R.id.draggable_frame)
     ElasticDragDismissFrameLayout mDraggableFrame;
+    @Bind(R.id.toolbar)
+    Toolbar mToolbar;
 
     private ElasticDragDismissFrameLayout.SystemChromeFader chromeFader;
 
@@ -25,10 +33,51 @@ public class MeiziDetialActivity extends BaseActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activiy_meizidetial);
         ButterKnife.bind(this);
-
         chromeFader = new ElasticDragDismissFrameLayout.SystemChromeFader(this);
 
+
+        initListener();
     }
+
+    private void initListener() {
+        if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.LOLLIPOP){//如果系统版本大于21
+            getWindow().getSharedElementEnterTransition().addListener(mTransitionListener);
+        }
+
+    }
+
+    private Transition.TransitionListener mTransitionListener=new Transition.TransitionListener() {
+
+        @Override
+        public void onTransitionStart(Transition transition) {
+           if (BuildConfig.DEBUG) Log.e("MeiziDetialActivity", "onTransitionStart");
+
+            mToolbar.animate().alpha(0).setDuration(5000).setInterpolator(new LinearOutSlowInInterpolator());
+
+        }
+
+        @Override
+        public void onTransitionEnd(Transition transition) {
+
+        }
+
+        @Override
+        public void onTransitionCancel(Transition transition) {
+
+        }
+
+        @Override
+        public void onTransitionPause(Transition transition) {
+
+        }
+
+        @Override
+        public void onTransitionResume(Transition transition) {
+
+        }
+    };
+
+
 
     @Override
     protected void onResume() {
