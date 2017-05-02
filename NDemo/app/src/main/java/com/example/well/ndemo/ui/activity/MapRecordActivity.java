cebfbe63684d3b68915a2679652514c040f62d92
niluogege <835350313@qq.com>
2017-05-02 14:24:58 +0800

    小人儿动画 完成

--- NDemo/app/src/main/java/com/example/well/ndemo/ui/activity/MapRecordActivity.java
+++ NDemo/app/src/main/java/com/example/well/ndemo/ui/activity/MapRecordActivity.java
@@ -4,7 +4,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -24,15 +23,17 @@ import com.amap.api.maps.model.PolylineOptions;
 import com.amap.api.trace.LBSTraceClient;
 import com.amap.api.trace.TraceListener;
 import com.amap.api.trace.TraceLocation;
-import com.example.well.ndemo.BuildConfig;
 import com.example.well.ndemo.R;
 import com.example.well.ndemo.bean.PathRecord;
 import com.example.well.ndemo.db.MapDbAdapter;
 import com.example.well.ndemo.utils.MapUtils;
 import com.example.well.ndemo.utils.SnackbarUtils;
+import com.example.well.ndemo.utils.TraceRePlay;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -52,6 +53,7 @@ public class MapRecordActivity extends BaseActivity {
     @Bind(R.id.rl_root)
     RelativeLayout rl_root;
 
+    private static final int SLEEPTIME = 20;//回调间隔时间
     private static final int TRACEID = 1;//纠偏轨迹ID
     public static final String EXTRAID = "recordId";
     private MenuItem mRecord_switch;//切换是纠偏路线还是未纠偏路线
@@ -66,6 +68,10 @@ public class MapRecordActivity extends BaseActivity {
     private Marker mStartMarker_trace;//纠偏轨迹的起点
     private Marker mEndMarker_trace;//纠偏轨迹的结束点
     private List<Integer> mColorList;
+    private ExecutorService mThreadPool;
+    private Marker mMarker_line_walker;
+    private Marker mMarker_trace_walker;
+    private TraceRePlay mRePlay;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -141,6 +147,12 @@ public class MapRecordActivity extends BaseActivity {
             mMap.setOnMapLoadedListener(mOnMapLoadedListener);//设计地图加载成功的回调
         }
         initColorList();
+        initTraceRePlay();
+    }
+
+    private void initTraceRePlay() {
+        int threadPoolSize = Runtime.getRuntime().availableProcessors() * 2 + 3;
+        mThreadPool = Executors.newFixedThreadPool(threadPoolSize);
     }
 
     /**
@@ -150,7 +162,6 @@ public class MapRecordActivity extends BaseActivity {
         int[] intArray = getResources().getIntArray(R.array.colorList);
         mColorList = new ArrayList<>();
         for (int i = 0; i < intArray.length; i++) {
-//            if (BuildConfig.DEBUG) Log.e("MapRecordActivity", "intArray[i]:" + intArray[i]);
             mColorList.add(intArray[i]);
         }
     }
@@ -191,7 +202,6 @@ public class MapRecordActivity extends BaseActivity {
             AMapLocation startpoint = record.getStartpoint();
             AMapLocation endpoint = record.getEndpoint();
             if (pathline == null || endpoint == null || startpoint == null) {
-                if (BuildConfig.DEBUG) Log.e("MapRecordActivity", "有数据为Null");
                 return;
             }
 
@@ -233,6 +243,9 @@ public class MapRecordActivity extends BaseActivity {
                 .colorValues(mColorList)
                 .zIndex(10)
                 .addAll(latLngList);
+        MarkerOptions options_walker = new MarkerOptions()
+                .position(startLatLng)
+                .icon(BitmapDescriptorFactory.fromResource(R.mipmap.run_big));
         MarkerOptions options_start = new MarkerOptions()
                 .position(startLatLng)
                 .icon(BitmapDescriptorFactory.fromResource(R.mipmap.point_start));
@@ -243,8 +256,10 @@ public class MapRecordActivity extends BaseActivity {
         mPoly_line = mMap.addPolyline(polylineOptions);
         mStartMarker_line = mMap.addMarker(options_start);
         mEndMarker_line = mMap.addMarker(options_end);
+        mMarker_line_walker = mMap.addMarker(options_walker);
         //newLatLngBounds-->返回CameraUpdate对象，这个对象包含一个经纬度限制的区域，并且是最大可能的缩放级别。 你可以设置一个边距数值来控制插入区域与view的边框之间的空白距离。 方法必须在地图初始化完成之后使用。
         mMap.moveCamera(CameraUpdateFactory.newLatLngBounds(bounds, 50));
+        reWark();
     }
 
     /**
@@ -263,6 +278,9 @@ public class MapRecordActivity extends BaseActivity {
         PolylineOptions polylineOptions = new PolylineOptions()
                 .width(20f)
                 .addAll(list);
+        MarkerOptions options_walker = new MarkerOptions()
+                .position(latLng_start)
+                .icon(BitmapDescriptorFactory.fromResource(R.mipmap.run_big));
         MarkerOptions options_start = new MarkerOptions()
                 .position(latLng_start)
                 .icon(BitmapDescriptorFactory.fromResource(R.mipmap.point_start));
@@ -272,8 +290,39 @@ public class MapRecordActivity extends BaseActivity {
         mPoly_trace = mMap.addPolyline(polylineOptions);
         mStartMarker_trace = mMap.addMarker(options_start);
         mEndMarker_trace = mMap.addMarker(options_end);
+        mMarker_trace_walker = mMap.addMarker(options_walker);
+
         //对纠偏轨迹进行隐藏
         updateMap();
+    }
+
+    private void reWark() {
+        if (mRePlay != null) {
+            mRePlay.stopTrace();
+        }
+        if (isTrace) {
+            mRePlay = startWark(tracePathList, mMarker_trace_walker);
+        } else {
+            mRePlay = startWark(mLatLngList, mMarker_line_walker);
+        }
+    }
+
+    private TraceRePlay startWark(List<LatLng> latLngList, final Marker marker) {
+        TraceRePlay rePlay = new TraceRePlay(latLngList, SLEEPTIME, new TraceRePlay.TraceRePlayListener() {
+            @Override
+            public void onTraceUpdating(LatLng latLng) {
+                if (marker != null) {
+                    marker.setPosition(latLng);
+                }
+            }
+
+            @Override
+            public void onTraceUpdateFinish() {
+
+            }
+        });
+        mThreadPool.execute(rePlay);
+        return rePlay;
     }
 
     /**
@@ -284,10 +333,12 @@ public class MapRecordActivity extends BaseActivity {
         mPoly_line.setVisible(!isTrace);
         mStartMarker_line.setVisible(!isTrace);
         mEndMarker_line.setVisible(!isTrace);
+        mMarker_line_walker.setVisible(!isTrace);
         //纠偏轨迹
         mPoly_trace.setVisible(isTrace);
         mStartMarker_trace.setVisible(isTrace);
         mEndMarker_trace.setVisible(isTrace);
+        mMarker_trace_walker.setVisible(isTrace);
     }
 
 
@@ -307,7 +358,7 @@ public class MapRecordActivity extends BaseActivity {
      * 纠偏轨迹的回调
      */
     TraceListener mTraceListener = new TraceListener() {
-        public List<LatLng> tracePathList;
+//        public List<LatLng> tracePathList;
 
         @Override
         public void onRequestFailed(int i, String s) {
@@ -321,9 +372,9 @@ public class MapRecordActivity extends BaseActivity {
 
         @Override
         public void onFinished(int i, List<LatLng> list, int i1, int i2) {
+            tracePathList = list;
             mRecord_switch.setChecked(true);
             drawTrace(list);
-            tracePathList = list;
 
         }
     };
@@ -343,6 +394,7 @@ public class MapRecordActivity extends BaseActivity {
                         SnackbarUtils.showDefaultLongSnackbar(rl_root, "已经切换到真实路线");
                     }
                     updateMap();
+                    reWark();
                     break;
             }
             return false;
